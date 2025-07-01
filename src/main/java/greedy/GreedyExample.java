934786dcee665260f25044fc5a917bf4c592150a
rngustj9139 <mnb9139@khu.ac.kr>
2025-07-01 15:24:14 +0900

    first commit

---
+++ src/main/java/greedy/GreedyExample.java
@@ -0,0 +1,16 @@
+/**
+ * public static void main(String[] args) {
+ *         System.out.println("Hello and welcome!");
+ * }
+ */
+package greedy;
+
+public class GreedyExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+    }
+
+}
+
+
